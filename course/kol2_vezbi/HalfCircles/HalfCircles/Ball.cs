d140883b3087c9145f3d25bacb23e39054804bd6
Mario Anchevski <marioancevski25@gmail.com>
2019-05-22 23:01:40 +0200

    initial commit

---
+++ course/kol2_vezbi/HalfCircles/HalfCircles/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfCircles
+{
+    public class Ball
+    {
+        public Point Center { get; set; }
+        public Color firstColor { get; set; }
+        public Color secondColor { get; set; }
+        public static int Radius = 30;
+        
+
+        public Ball(Point p) {
+            firstColor = Color.Red;
+            secondColor = Color.LightBlue;
+            Center = p;
+        }
+
+        public void draw(Graphics g) {
+            Brush b = new SolidBrush(firstColor);
+            Brush b2 = new SolidBrush(secondColor);
+            g.FillPie(b, Center.X - Radius, Center.Y - Radius, 2 * Radius, 2 * Radius, 0, 180);
+            g.FillPie(b2, Center.X - Radius, Center.Y - Radius, 2 * Radius, 2 * Radius, 180, 180);
+            b.Dispose();
+            b2.Dispose();
+
+        }
+
+        public void pulse() {
+            Radius += 3;
+            if (Radius == 60)
+                Radius = 30;
+        }
+    }
+}
---
+++ course/kol2_vezbi/HalfCircles/HalfCircles/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalfCircles
+{
+    public partial class Form1 : Form
+    {
+        public BallDoc topcinja { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            topcinja = new BallDoc();
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            topcinja.draw(e.Graphics);
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) {
+                topcinja.addBall(new Ball(e.Location));
+                Invalidate(true);
+            }
+        }
+
+        private void statusStrip1_Paint(object sender, PaintEventArgs e)
+        {
+            toolStripStatusLabel1.Text = string.Format("Total:{0}",topcinja.items.Count);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            topcinja.pulse();
+            Invalidate(true);
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
